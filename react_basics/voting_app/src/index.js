0557ae3d214638e7b402ba88ecef7700b3abea1a
KOSINSKI, COLLIN <collinkosinski@northwesternmutual.com>
2017-06-02 10:34:20 -0500

    finished voting_app with sort

--- react_basics/voting_app/src/index.js
+++ react_basics/voting_app/src/index.js
@@ -3,49 +3,57 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import imagePath from './Image/plus.png';
 
-const library = [
-    {
-        language: 'React',
-        voteCount: 0
-    },
-    {
-        language: 'Vue',
-        voteCount: 0
-        
-        
-    },
-    {
-        language: 'Angular',
-        voteCount: 0
-        
 
-    },
-    {
-        language: 'Ember',
-        voteCount: 0
+
+class App extends React.Component {
+    state = {
+        library: [
+            {
+                language: 'React',
+                voteCount: 0
+            },
+            {
+                language: 'Vue',
+                voteCount: 0
+
+
+            },
+            {
+                language: 'Angular',
+                voteCount: 0
+
+
+            },
+            {
+                language: 'Ember',
+                voteCount: 0}
+        ]
+
+    }
     
 
-    }
-];
+    updateVoteCount = (library) => {
+        const newLibrary = this.state.library.map((libObj, index) => {
+            if(libObj.language === library ){
+                return{
+                    ...libObj,
+                    voteCount: libObj.voteCount +1
+                }
+            }
+            return libObj;
+        });
 
-class App extends React.Component{
-    state ={
-        'voteCount': library.voteCount
-    }
+        this.setState({
+            library: newLibrary.sort(( a,b ) => b.voteCount - a.voteCount)
 
-    updateVoteCount=(update) =>{
-        this.setState(() =>{
-            return(
-           {'voteCount': this.voteCount +1}
-
-            )
         })
     }
+
     render() {
         return (
             <div className="header">
                 <h1>Vote Your JS Library!</h1>
-                <CountList library={library} count={this.state.voteCount}/>
+                <CountList library={this.state.library} onClick={this.updateVoteCount} />
             </div>
         )
     }
@@ -53,10 +61,8 @@ class App extends React.Component{
 
 
 const CountList = (props) => {
-    const {count} = props.state.voteCount;
-    console.log(props.count)
     const library = props.library.map((counterObj, idx) => {
-        return <Counter key={idx} language={counterObj.language} voteCount={counterObj.voteCount} count={props.voteCount}/>;
+        return <Counter key={idx} language={counterObj.language} voteCount={counterObj.voteCount} onClick={props.onClick} />;
     });
 
     return (
@@ -68,14 +74,16 @@ const CountList = (props) => {
 
 const Counter = (props) => {
     const { language } = props;
-    const {voteCount}= props;
-    const {count} = props;
-    console.log(count)
+    const { voteCount } = props;
+    const handleClick = () =>{
+        props.onClick(props.language)
+    }
+
     return (
         <div className="counter">
             <p className="numberContainer">{voteCount}</p>
             <p className="language">{language}</p>
-            <img src={imagePath} alt="plus sign"  />
+            <img src={imagePath} alt="plus sign" onClick={handleClick} />
         </div>
     )
 }
@@ -83,4 +91,4 @@ const Counter = (props) => {
 
 
 
-ReactDOM.render(<App  />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
