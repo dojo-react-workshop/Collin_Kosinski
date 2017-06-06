d5b37b78cd0106673658d5ee27092f130559fe96
KOSINSKI, COLLIN <collinkosinski@northwesternmutual.com>
2017-06-06 10:30:10 -0500

    adding error with setting the state in the Form

--- react_basics/exam_practice/src/index.js
+++ react_basics/exam_practice/src/index.js
@@ -1,28 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    return(
-        <div>
-            <h1> Bootcamp Repo App </h1>
-            <Form />
-        </div>
-    )
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            repo_name: '',
+            description: ''
+        }
+    }
+    render() {
+        return (
+            <div>
+                <h1> Bootcamp Repo App </h1>
+                <Form />
+            </div>
+        )
+    }
+
 }
 
-const Form = () => {
-    return(
-        <div>
-            <form>
-                <input type="text" name="name" placeholder="name" />
-                <input type="submit" name="submit" placeholder="Submit" />
-             </form>
-        </div>
-    )
+class Form extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+    onHandle = (event) =>{
+        const name =event.target.name
+        const value = event.target.value
+        console.log(name, value)
+        this.setState({
+            [name]: value
+        })
+    }
+    render() {
+        return (
+            <div>
+                <form>
+                    <input type="text" name="repo_name" placeholder="Repo Name" onChange={this.onHandle}/>
+                    <input type="textarea" name="description" placeholder="description" onChange={this.onHandle}/>                    
+                    <input type="submit" name="submit" placeholder="Submit" />
+                </form>
+            </div>
+        )
+    }
 }
 
 const FormResults = () => {
-    return null 
+    return null
 }
 
 const RouterResults = () => {
